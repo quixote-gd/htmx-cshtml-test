a85a4fd4d83da7857d4a9a955792bcd5af3379bd
george <george@georges-Mac-mini.local>
2023-07-31 20:28:15 -0400

    form validatin

--- HtmxApp/Controllers/IndexController.cs
+++ HtmxApp/Controllers/IndexController.cs
@@ -13,6 +13,24 @@ public class IndexController : Controller
         count++;
         return Ok(count);
     }
+    
+    [HttpPost("ValidateEmail")]
+    [ActionName("ValidateEmail")]
+    public IActionResult ValidateEmail(string email)
+    {
+
+        // this can easily be a partial view.
+
+        var response = """
+        <div hx-target="this" hx-swap="outerHTML" class="error">
+        <label>Email Address</label>
+        <input name="email" hx-post="/contact/email" hx-indicator="#ind" value="test@foo.com">
+        <img id="ind" src="/img/bars.svg" class="htmx-indicator"/>
+        <div class='error-message'>That email is already taken.  Please enter another email.</div>
+        </div>
+        """;
+        return Ok(response);
+    }
 
 
 }
